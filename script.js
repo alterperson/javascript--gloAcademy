f1cf23373ecaa1c3260896b8ef45b43a4401acf2
Vadim Melnichuk <vad.mk@ya.ru>
2021-11-26 20:52:56 +0300

    Create lesson02 branch

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const title;
-const screens;
-const screenPrice;
-const rollback;
-const fullPrice;
-const adaptive;
+const title = "JavaScript";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 7;
+const rollback = 11;
+const fullPrice = 99;
+const adaptive = true;
 
 alert("Любой текст");
 console.log("Любой текст");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " долларов" + " и " + "Стоимость разработки сайта " + fullPrice + " долларов");
+console.log(screens.toLocaleLowerCase().split(", "));
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
